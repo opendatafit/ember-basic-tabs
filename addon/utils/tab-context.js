d230f5ac32a6402e0e14be5343b1d916a7fddfe8
James Wilmot <jameswilmot2000@gmail.com>
2022-04-20 14:27:58 +1000

    Added TabContext util class

---
+++ addon/utils/tab-context.js
@@ -0,0 +1,25 @@
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class TabContext {
+  @tracked _tabsContent = [];
+  @tracked _selectedIndex = 0;
+
+  constructor(tabs, selected=0) {
+    this._tabsContent = tabs;
+    this._selectedIndex = selected;
+  }
+
+  get tabs() {
+    return this._tabsContent;
+  }
+
+  get selectedIndex() {
+    return this._selectedIndex;
+  }
+
+  @action
+  handleButtonClick(index) {
+    this._selectedIndex = index;
+  }
+}
---
+++ tests/unit/utils/tab-context-test.js
@@ -0,0 +1,11 @@
+import tabContext from 'dummy/utils/tab-context';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | tab-context', function() {
+
+  // TODO: Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = tabContext();
+    assert.ok(result);
+  });
+});
