dff09dad54089550a43158792d38b71eeee042ce
James Wilmot <jameswilmot2000@gmail.com>
2023-04-06 22:36:06 +1000

    dummy application: added another tabstate

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -29,6 +29,33 @@ export default class ApplicationController extends Controller {
               "content": "some content 4"
             }
           ];
+  
+  secondaryTabs = [
+            {
+              "name": "tab1",
+              "title": "Tab 1",
+              "icon": "",
+              "content": "some further content 1"
+            },
+            {
+              "name": "tab2",
+              "title": "Tab 2",
+              "icon": "",
+              "content": "some further content 2"
+            },
+            {
+              "name": "tab3",
+              "title": "Tab 3",
+              "icon": "",
+              "content": "some further content 3"
+            },
+            {
+              "name": "tab4",
+              "title": "Tab 4",
+              "icon": "",
+              "content": "some further content 4"
+            }
+          ];
  
   myTabState = null;
 
@@ -36,6 +63,7 @@ export default class ApplicationController extends Controller {
     super(...arguments);
 
     this.myTabState = new TabState(this.tabs);
+    this.secondaryTabState = new TabState(this.secondaryTabs);
   }
   
 }
