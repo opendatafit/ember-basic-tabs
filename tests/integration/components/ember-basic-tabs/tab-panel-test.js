608f7ce80aebe47f49669dfa591b09fda6194e0f
James Wilmot <jameswilmot2000@gmail.com>
2023-04-13 17:28:22 +1000

    tests: tab-item-test and tab-panel-test working correctly

--- tests/integration/components/ember-basic-tabs/tab-panel-test.js
+++ tests/integration/components/ember-basic-tabs/tab-panel-test.js
@@ -3,20 +3,24 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
+import TabState from '@opendatafit/ember-basic-tabs/utils/tab-state';
+
 module('Integration | Component | ember-basic-tabs/tab-panel', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    let myTabState = new TabState();
+    this.set('myTabState', myTabState);
 
-    await render(hbs`<EmberBasicTabs::TabPanel />`);
+    await render(hbs`<EmberBasicTabs::TabPanel @tabState={{this.myTabState}} />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <EmberBasicTabs::TabPanel>
+      <EmberBasicTabs::TabPanel @tabState={{this.myTabState}}>
         template block text
       </EmberBasicTabs::TabPanel>
     `);
